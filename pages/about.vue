a383225646194c828aeaf68eea1de64fc63009b0
Igor Chernyak <i_chernyak@inbox.ru>
2021-04-25 13:28:14 +0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+    <section>
+        <h1>About page</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta illo sapiente natus tenetur, veniam obcaecati quidem totam.
+            Porro reprehenderit nemo molestiae? Porro, aliquam nulla. Fugiat quis placeat excepturi autem voluptates!</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta illo sapiente natus tenetur, veniam obcaecati quidem totam.
+            Porro reprehenderit nemo molestiae? Porro, aliquam nulla. Fugiat quis placeat excepturi autem voluptates!</p>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </section>
+</template>
+
+<script>
+export default {
+    middleware: ['auth']
+}
+</script>
